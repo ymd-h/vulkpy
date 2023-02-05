51a75d339c5a3e7b3779f5ee934a4793476647e9
Yamada <ymd_h@outlook.jp>
2023-02-05 11:33:09 +0900

    Enable api_dump

--- vulkpy/util.py
+++ vulkpy/util.py
@@ -1,5 +1,15 @@
 import os
 
 
-def enable_debug():
-    os.environ["VK_INSTANCE_LAYERS"] = "VK_LAYER_KHRONOS_validation"
+def enable_debug(*, validation=True, api_dump=True):
+    """
+    Enable debug message
+    """
+    layers = []
+    if validation:
+        layers.append("VK_LAYER_KHRONOS_validation")
+    if api_dump:
+        layers.append("VK_LAYER_LUNARG_api_dump")
+
+    if len(layers) > 0:
+        os.environ["VK_INSTANCE_LAYERS"] = ":".join(layers)
