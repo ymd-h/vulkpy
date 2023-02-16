7c2b88649c2ef14260a7424fc208898753c3d37f
Yamada <ymd_h@outlook.jp>
2023-02-16 18:50:13 +0900

    Refactoring: Make mixin class for PRNG conversion

--- vulkpy/random.py
+++ vulkpy/random.py
@@ -11,61 +11,9 @@ from .util import getShader
 
 __all__ = ["Xoshiro128pp"]
 
-class Xoshiro128pp:
-    _spv = getShader("prng_xoshiro128pp.spv")
+class _ConvertMixin:
     _box_muller = getShader("prng_box_muller.spv")
     _ibox_muller = getShader("prng_ibox_muller.spv")
-
-    def __init__(self, gpu: vk.GPU, size: int = 64, *, seed: Optional[int] = None):
-        """
-        xoshiro128++: Pseudo Random Number Generator
-
-        Parameters
-        ----------
-        gpu : vulkpy.GPU
-            GPU where PRNG allocates
-        size : int
-            Number of internal states. These states generate random number parallelly.
-        seed : int, optional
-            Random seed. If ``None`` (default), use hardware random instead.
-        """
-        self._gpu = gpu
-
-        if seed is None:
-            self.rng = _vkarray.Xoshiro128pp(self._gpu.gpu, self._spv, size)
-        else:
-            self.rng = _vkarray.Xoshiro128pp(self._gpu.gpu, self._spv, size, seed)
-
-    def random(self, *,
-               shape: Optional[Iterable[int]] = None,
-               buffer: Optional[vk.Array] = None) -> vk.Array:
-        """
-        Generate [0, 1) floating numbers
-
-        Parameters
-        ----------
-        shape : iterable of ints, optional
-            If specified, new ``vulkpy.Array`` with ``shape`` will be returned.
-        buffer : vulkpy.Array
-            If specified, generated numbers will be stored.
-
-        Returns
-        -------
-        vulkpy.Array
-            Array which will get random numbers.
-        """
-        if buffer is None:
-            if shape is None:
-                raise ValueError("One of `shape` and  `buffer` must be specified.")
-
-            buffer = vk.Array(self._gpu, shape=shape)
-        else:
-            # For safety, we wait output buffer job.
-            buffer.wait()
-
-        n = int(np.prod(buffer.shape))
-        buffer.job = self.rng.random(n, buffer.buffer.info())
-        return buffer
 
     def normal(self, *,
                shape: Optional[Iterable[int]] = None,
@@ -128,4 +76,58 @@ class Xoshiro128pp:
                                            [rng.buffer, buffer.buffer],
                                            dshape, p, [rng.job])
 
+        return buffer
+
+class Xoshiro128pp(_ConvertMixin):
+    _spv = getShader("prng_xoshiro128pp.spv")
+
+    def __init__(self, gpu: vk.GPU, size: int = 64, *, seed: Optional[int] = None):
+        """
+        xoshiro128++: Pseudo Random Number Generator
+
+        Parameters
+        ----------
+        gpu : vulkpy.GPU
+            GPU where PRNG allocates
+        size : int
+            Number of internal states. These states generate random number parallelly.
+        seed : int, optional
+            Random seed. If ``None`` (default), use hardware random instead.
+        """
+        self._gpu = gpu
+
+        if seed is None:
+            self.rng = _vkarray.Xoshiro128pp(self._gpu.gpu, self._spv, size)
+        else:
+            self.rng = _vkarray.Xoshiro128pp(self._gpu.gpu, self._spv, size, seed)
+
+    def random(self, *,
+               shape: Optional[Iterable[int]] = None,
+               buffer: Optional[vk.Array] = None) -> vk.Array:
+        """
+        Generate [0, 1) floating numbers
+
+        Parameters
+        ----------
+        shape : iterable of ints, optional
+            If specified, new ``vulkpy.Array`` with ``shape`` will be returned.
+        buffer : vulkpy.Array
+            If specified, generated numbers will be stored.
+
+        Returns
+        -------
+        vulkpy.Array
+            Array which will get random numbers.
+        """
+        if buffer is None:
+            if shape is None:
+                raise ValueError("One of `shape` and  `buffer` must be specified.")
+
+            buffer = vk.Array(self._gpu, shape=shape)
+        else:
+            # For safety, we wait output buffer job.
+            buffer.wait()
+
+        n = int(np.prod(buffer.shape))
+        buffer.job = self.rng.random(n, buffer.buffer.info())
         return buffer
