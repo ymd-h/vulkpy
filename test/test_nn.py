a43195a192b62b221e3765b61f048c9d61a19e28
Yamada <ymd_h@outlook.jp>
2023-02-26 20:29:11 +0900

    Test: ReLU.forward

---
+++ test/test_nn.py
@@ -0,0 +1,24 @@
+import unittest
+
+import numpy as np
+
+import vulkpy as vk
+from vulkpy import nn
+
+
+class TestNN(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.gpu = vk.GPU()
+
+    def test_relu_forward(self):
+        relu = nn.ReLU()
+
+        x = vk.Array(self.gpu, data=[[-0.2, 0.0, 0.2]])
+        y = relu(x)
+
+        np.testing.assert_allclose(y, [[0.0, 0.0, 0.2]])
+
+
+if __name__ == "__main__":
+    unittest.main()
