48f5ce5622300f341f5aea1bf15fdd274528d6f3
Yamada <ymd_h@outlook.jp>
2023-02-26 22:44:25 +0900

    test for sigmoid

--- test/test_nn.py
+++ test/test_nn.py
@@ -30,6 +30,29 @@ class TestNN(unittest.TestCase):
 
         np.testing.assert_allclose(dx, [[0.0, 0.0, 0.9]])
 
+    def test_sigmoid_forward(self):
+        sigmoid = nn.Sigmoid()
+
+        d = np.asarray([[-100, -0.1, 0, 10,  100]])
+        x = vk.Array(self.gpu, data=d)
+
+        y = sigmoid(x)
+
+        np.testing.assert_allclose(y, 1/(1+np.exp(-d)), rtol=1e-7, atol=1e-7)
+
+    def test_sigmoid_backward(self):
+        sigmoid = nn.Sigmoid()
+
+        _x = np.asarray([[-100, -0.1, 0, 10,  100]])
+        x = vk.Array(self.gpu, data=_x)
+        y = sigmoid(x)
+
+        _dy = np.asarray([[0.1, 0.2, 0.3, 0.5, 0.7]])
+        dy = vk.Array(self.gpu, data=_dy)
+
+        dx = sigmoid.backward(dy)
+        np.testing.assert_allclose(dx, dy * y * (1 - y))
+
     def test_he(self):
         seed = 645
         shape = (10,)
