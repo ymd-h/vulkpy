986a4044b157aae112ecaeae57bb4961b3a569a2
Yamada <ymd_h@outlook.jp>
2023-02-26 22:22:28 +0900

    docstring for HeNormal

--- vulkpy/nn.py
+++ vulkpy/nn.py
@@ -15,14 +15,26 @@ from .util import getShader
 
 class HeNormal:
     """
-    He normal initializer
+    He Normal Initializer
 
     Note
     ----
     .. math:: \sigma = \sqrt(2/input_dim)
     """
-    def __init__(self, gpu: GPU, input_dim: int):
-        self.rng = Xoshiro128pp(gpu)
+    def __init__(self, gpu: GPU, input_dim: int, *, seed: Optional[int] = None):
+        """
+        Initialize He Normal Initializer
+
+        Parameters
+        ----------
+        gpu : vulkpy.GPU
+            GPU
+        input_dim : int
+            Input dimension
+        seed : int, optional
+            Initial seed for PRNG
+        """
+        self.rng = Xoshiro128pp(gpu, seed=seed)
         self.stddev = np.sqrt(2 / input_dim)
 
     def __call__(self, gpu: GPU, shape: Iterable[int]):
