fdfdd6ba90f241c49407e9c6fa27940ad77a6281
Yamada <ymd_h@outlook.jp>
2023-02-28 23:13:35 +0900

    Add docstring

--- vulkpy/nn/optimizers.py
+++ vulkpy/nn/optimizers.py
@@ -16,13 +16,59 @@ class SGDState:
     pass
 
 class SGD:
+    """
+    Stachostic Gradient Decent Optimizer
+
+    Use constant learning rate
+    """
     def __init__(self, lr: float):
+        """
+        Initialize SGD
+
+        Parameters
+        ----------
+        lr : float
+            Learning rate
+        """
         self.lr = lr
 
     def __call__(self, grad: Array, state: SGDState) -> Array:
+        """
+        Compute update diff from gradient
+
+        Parameters
+        ----------
+        grad : vulkpy.Array
+            Gradient
+        state : SGDState
+            Optimizer state
+
+        Returns
+        -------
+        vulkpy.Array
+            Update diff
+        """
         return (-self.lr) * grad
 
     def init_state(self, shape: Iterable[int]) -> SGDState:
+        """
+        Initialize Optimizer state
+
+        Parameters
+        ----------
+        shape : iterable of ints
+            Shape of parameter
+
+        Returns
+        -------
+        SGDState
+            Optimizer state
+
+        Notes
+        -----
+        Currently SGDState is empty, however,
+        we might add some field like momentum in future.
+        """
         return SGDState()
 
 
@@ -34,12 +80,27 @@ class AdamState:
     beta2t: float = 1.0
 
 class Adam:
+    """
+    Adam Optimizer
+    """
     def __init__(self,
                  gpu: GPU,
                  lr: float = 0.001,
                  beta1: float = 0.9,
                  beta2: float = 0.999,
                  eps: float = 1e-8):
+        """
+        Initialize Adam Optimizer
+
+        Parameters
+        ----------
+        gpu : vulkpy.GPU
+            GPU
+        lr : float, optional
+            Learning rate. The default is 0.001
+        beta1, beta2, eps : float, optional
+            Adam parameters. The defaults are 0.9, 0.999, 1e-8, respectively.
+        """
         self.gpu = gpu
         self.lr = lr
         self.beta1 = beta1
@@ -47,6 +108,21 @@ class Adam:
         self.eps = eps
 
     def __call__(self, grad: Array, state: AdamState) -> Array:
+        """
+        Compute update diff from gradient
+
+        Parameters
+        ----------
+        grad : vulkpy.Array
+            Gradient
+        state : AdamState
+            Optimizer state
+
+        Returns
+        -------
+        vulkpy.Array
+            Update diff
+        """
         state.m *= self.beta1
         state.m += (1 - self.beta1) * grad # Allocate
 
@@ -68,7 +144,20 @@ class Adam:
         return mhat
 
     def init_state(self, shape: Iterable[int]) -> AdamState:
-        state = AdamState(m=Array(self.gpu, shape=shape),
+        """
+        Initialize Optimizer state
+
+        Parameters
+        ----------
+        shape : iterable of ints
+            Shape of parameter
+
+        Returns
+        -------
+        AdamState
+            Optimizer state
+        """
+       state = AdamState(m=Array(self.gpu, shape=shape),
                           v=Array(self.gpu, shape=shape))
         state.m[:] = 0
         state.v[:] = 0
