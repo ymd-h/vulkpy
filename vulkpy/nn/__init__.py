95f6685a6e4848853b5b324688c9db623bf9c0ac
Yamada <ymd_h@outlook.jp>
2023-03-02 07:31:12 +0900

    Export Losses

--- vulkpy/nn/__init__.py
+++ vulkpy/nn/__init__.py
@@ -7,3 +7,4 @@ Neural Network Module (:mod:`vulkpy.nn`)
 from .initializers import Constant, HeNormal
 from .optimizers import SGD, Adam
 from .layers import Dense, ReLU, Sigmoid, Softmax
+from .losses import SoftmaxCrossEntropyLoss, MSELoss, HuberLoss
