20739398548e5e87a3c122b2563a5fef49670693
Yamada <ymd_h@outlook.jp>
2023-03-06 19:09:19 +0900

    docstring for losses

--- vulkpy/nn/losses.py
+++ vulkpy/nn/losses.py
@@ -20,12 +20,39 @@ class Loss:
         }[reduce]
 
     def __call__(self, x: Array, y: Array) -> Array:
+        r"""
+        Compute Loss
+
+        Parameters
+        ----------
+        x : vulkpy.Array
+            Batch input features
+        y : vulkpy.Array
+            Batch labels/targets
+
+        Returns
+        -------
+        loss : vulkpy.Array
+            Loss
+        """
         self._x = x
         self._y = y
         L = self.forward(x, y)
         return self.reduce(L)
 
-    def grad(self):
+    def grad(self) -> Array:
+        r"""
+        Compute Gradients
+
+        Returns
+        -------
+        dx : vulkpy.Array
+            Batch gradients of dL/dx
+
+        Notes
+        -----
+        This method calculates gradients for the last ``__call__(x, y)``.
+        """
         dx = self.backward()
         if self.scale_backward is not None:
             dx *= self.scale_backward(dx)
@@ -46,9 +73,43 @@ class CrossEntropyLoss(Loss):
     _backward = getShader("nn_cross_entropy_backward.spv")
 
     def __init__(self, *args, **kwargs):
+        """
+        Initialize Cross Entropy Loss
+
+        Parameters
+        ----------
+        reduce : {"mean", "sum"}, optional
+            Reduction method over batch. The default is ``"mean"`.
+        """
         super().__init__(*args, **kwargs)
 
-    def forward(self, x: Array, y: Array):
+    def forward(self, x: Array, y: Array) -> Array:
+        r"""
+        Forward
+
+        Parameters
+        ----------
+        x : vulkpy.Array
+            Batch input features
+        y : vulkpy.Array
+            Batch input labels as One hot vector
+
+        Returns
+        -------
+        loss : vulkpy.Array
+            Cross Entropy Loss
+
+        Notes
+        -----
+        .. math::
+
+             L = - Reduce _i ( y_i \times \log (x_i) )
+
+        .. warning::
+
+             Generally, users should not call this method directly.
+             Use ``__call__`` instead, where input / output are stored for training.
+        """
         size = x.buffer.size()
         L = Array(x._gpu, shape=x.shape)
         L.job = x._gpu._submit(self._forward, 64, 1, 1,
@@ -58,7 +119,26 @@ class CrossEntropyLoss(Loss):
         L._keep.extend([x, y])
         return L.sum(axis=1)
 
-    def backward(self):
+    def backward(self) -> Array:
+        r"""
+        Backward
+
+        Returns
+        -------
+        loss : vulkpy.Array
+           Batch gradients
+
+        Notes
+        -----
+        .. math::
+
+             dx = \frac{-y}{x + \epsilon}
+
+        .. warning::
+
+             Generally, users should not call this method directly.
+             Use ``grad()`` instead, where reduction scale is corrected.
+        """
         size = self._x.buffer.size()
         dx = Array(self._x._gpu, shape=self._x.shape)
         dx.job = self._x._gpu._submit(self._backward, 64, 1, 1,
@@ -79,13 +159,66 @@ class SoftmaxCrossEntropyLoss(CrossEntropyLoss):
     CrossEntropyLoss : Cross Entropy loss without Softmax
     """
     def __init__(self, *args, **kwargs):
+        """
+        Initialize Softmax Cross Entropy Loss
+
+        Parameters
+        ----------
+        reduce : {"mean", "sum"}
+            Reduction method over batch. The default is ``"mean"``.
+        """
         super().__init__(*args, **kwargs)
         self._sm = Softmax()
 
     def forward(self, x: Array, y: Array) -> Array:
+        r"""
+        Forward
+
+        Parameters
+        ----------
+        x : vulkpy.Array
+            Batch input features
+        y : vulkpy.Array
+            Batch labels
+
+        Returns
+        -------
+        loss : vulkpy.Array
+            Loss
+
+        Notes
+        -----
+        .. math::
+
+             L = - Reduce _i (y_i \times \log (softmax(x) _i))
+
+        .. warning::
+
+             Generally, users should not call this method directly.
+             Use ``__call__`` instead, where input / output are stored for training.
+        """
         return super().forward(self._sm(x), y)
 
     def backward(self) -> Array:
+        r"""
+        Backward
+
+        Returns
+        -------
+        loss : vulkpy.Array
+           Batch gradients
+
+        Notes
+        -----
+        .. math::
+
+             dx = softmax(x) - y
+
+        .. warning::
+
+             Generally, users should not call this method directly.
+             Use ``grad()`` instead, where reduction scale is corrected.
+        """
         return self._sm._y - self._y
 
 
@@ -94,14 +227,67 @@ class MSELoss(Loss):
     Mean Squared Loss
     """
     def __init__(self, *args, **kwargs):
+        """
+        Initialize MSE Loss
+
+        Parameters
+        ----------
+        reduce : {"mean", "sum"}
+            Reduction method over batch. The default is ``"mean"``.
+        """
         super().__init__(*args, **kwargs)
 
     def forward(self, x: Array, y: Array) -> Array:
+        r"""
+        Forward
+
+        Parameters
+        ----------
+        x : vulkpy.Array
+            Batch input features
+        y : vulkpy.Array
+            Batch labels
+
+        Returns
+        -------
+        loss : vulkpy.Array
+            Loss
+
+        Notes
+        -----
+        .. math::
+
+             L = Reduce _i |x - y|^2
+
+        .. warning::
+
+             Generally, users should not call this method directly.
+             Use ``__call__`` instead, where input / output are stored for training.
+        """
         L = (y - x)          # Allocate
         L **= 2.0
         return L.sum(axis=1) # Allocate
 
     def backward(self) -> Array:
+        r"""
+        Backward
+
+        Returns
+        -------
+        loss : vulkpy.Array
+           Batch gradients
+
+        Notes
+        -----
+        .. math::
+
+             dx = 2 * (x - y)
+
+        .. warning::
+
+             Generally, users should not call this method directly.
+             Use ``grad()`` instead, where reduction scale is corrected.
+        """
         dx = self._x - self._y # Allocate
         dx *= 2
         return dx
@@ -112,9 +298,43 @@ class HuberLoss(Loss):
     Huber Loss
     """
     def __init__(self, *args, **kwargs):
+        """
+        Initialize Huber Loss
+
+        Parameters
+        ----------
+        reduce : {"mean", "sum"}
+            Reduction method over batch. The default is ``"mean"``.
+        """
         super().__init__(*args, **kwargs)
 
     def forward(self, x: Array, y: Array) -> Array:
+        r"""
+        Forward
+
+        Parameters
+        ----------
+        x : vulkpy.Array
+            Batch input features
+        y : vulkpy.Array
+            Batch labels
+
+        Returns
+        -------
+        loss : vulkpy.Array
+            Loss
+
+        Notes
+        -----
+        .. math::
+
+             L = 0.5 Reduce _i min(|x - y|^2, |x - y|)
+
+        .. warning::
+
+             Generally, users should not call this method directly.
+             Use ``__call__`` instead, where input / output are stored for training.
+        """
         delta = y - x # Allocate
         delta.abs(inplace=True)               # |y-x|
         delta.min(delta ** 2.0, inplace=True) # min(|y-x|^2, |y-x|)
@@ -122,6 +342,25 @@ class HuberLoss(Loss):
         return delta.sum(axis=1) # Allocate
 
     def backward(self) -> Array:
+        r"""
+        Backward
+
+        Returns
+        -------
+        loss : vulkpy.Array
+           Batch gradients
+
+        Notes
+        -----
+        .. math::
+
+             dx = clamp(x - y, -1.0, 1.0)
+
+        .. warning::
+
+             Generally, users should not call this method directly.
+             Use ``grad()`` instead, where reduction scale is corrected.
+        """
         delta = self._x - self._y
         delta.clamp(-1.0, 1.0, inplace=True)
         return delta
