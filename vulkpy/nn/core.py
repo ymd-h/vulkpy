449a1d112498bfd98a1c6a6cd0b135664e122bb0
Yamada <ymd_h@outlook.jp>
2023-03-12 11:13:53 +0900

    Add: MixLoss
    Refactoring: Divide Loss to Loss (base class) and ReduceLoss

--- vulkpy/nn/core.py
+++ vulkpy/nn/core.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Callable, Literal, Optional, Iterable, Tuple
+from typing import Iterable
 
 from vulkpy.vkarray import GPU, Array
 
@@ -73,57 +73,10 @@ class Module:
         """
         pass
 
-F = Callable[[Array], Array]
+
 class Loss:
-    def __init__(self, reduce: Literal["mean", "sum"] = "mean"):
-        tmp: Tuple[F, Optional[F]] = {
-            "mean": (lambda _L: _L.mean(axis=0), lambda _dx: 1/_dx.shape[0]),
-            "sum": (lambda _L: _L.sum(axis=0), None),
-        }[reduce]
-        self.reduce, self.scale_backward = tmp
-
-
     def __call__(self, x: Array, y: Array) -> Array:
-        r"""
-        Compute Loss
-
-        Parameters
-        ----------
-        x : vulkpy.Array
-            Batch input features
-        y : vulkpy.Array
-            Batch labels/targets
-
-        Returns
-        -------
-        loss : vulkpy.Array
-            Loss
-        """
-        self._x = x
-        self._y = y
-        L = self.forward(x, y)
-        return self.reduce(L)
-
-    def grad(self) -> Array:
-        r"""
-        Compute Gradients
-
-        Returns
-        -------
-        dx : vulkpy.Array
-            Batch gradients of dL/dx
-
-        Notes
-        -----
-        This method calculates gradients for the last ``__call__(x, y)``.
-        """
-        dx = self.backward()
-        if self.scale_backward is not None:
-            dx *= self.scale_backward(dx)
-        return dx
-
-    def forward(self, x: Array, y: Array) -> Array:
         raise NotImplementedError
 
-    def backward(self) -> Array:
+    def grad(self) -> Array:
         raise NotImplementedError
