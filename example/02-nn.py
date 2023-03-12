5862e516b5d4cd4541e732baf263dd25125c78b6
Yamada <ymd_h@outlook.jp>
2023-03-12 17:10:30 +0900

    Support Regularization argument

--- example/02-nn.py
+++ example/02-nn.py
@@ -15,7 +15,7 @@ from vulkpy.util import enable_debug
 from vulkpy import nn
 
 
-def example02(nepoch, batch_size, opt, lr, *, debug = False):
+def example02(nepoch, batch_size, opt, lr, l1, l2, *, debug = False):
     if debug:
         enable_debug(api_dump=False)
 
@@ -42,7 +42,13 @@ def example02(nepoch, batch_size, opt, lr, *, debug = False):
         "sgd": lambda lr: nn.SGD(lr)
     }[opt](lr)
 
-    R = nn.Elastic(L1=0.01, L2=0.01)
+    R = None
+    if (l1 is not None) and (l2 is not None):
+        R = nn.Elastic(l1, l2)
+    elif (l1 is not None):
+        R = nn.Lasso(l1)
+    elif (l2 is not None):
+        R = nn.Ridge(l2)
 
     # Sequential Model: 4 -> 128 -> 128 -> 3
     net = nn.Sequence(
@@ -99,10 +105,14 @@ if __name__ == "__main__":
     p.add_argument("--debug", action="store_true")
     p.add_argument("--optimizer", choices=["adam", "sgd"], default="adam")
     p.add_argument("--learning-rate", type=float, default=0.0001)
+    p.add_argument("--l1", type=float, help="L1 regularization", default=None)
+    p.add_argument("--l2", type=float, help="L2 regularization", default=None)
     p = p.parse_args()
 
     example02(nepoch=p.nepoch,
               batch_size=p.batch_size,
               opt=p.optimizer,
               lr=p.learning_rate,
+              l1=p.l1,
+              l2=p.l2,
               debug=p.debug)
